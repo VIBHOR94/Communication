9de7e887d1f25dd715dcd8b454231ab6b5c17927
TANMAYA <vibhorgupta2008@aol.in>
2018-04-02 02:23:25 +0530

    Functionality of closing and changing label added

--- communication.py
+++ communication.py
@@ -7,15 +7,20 @@ class HelloWorld(QDialog):
   def __init__(self):
     QDialog.__init__(self)
     layout = QVBoxLayout()
-    label = QLabel("Hello World")
+    self.label = QLabel("Hello World")
     line_edit = QLineEdit()
     button = QPushButton("Close")
 
-    layout.addWidget(label)
+    layout.addWidget(self.label)
     layout.addWidget(line_edit)
     layout.addWidget(button)
 
     self.setLayout(layout)
+    button.clicked.connect(self.close)
+    line_edit.textChanged.connect(self.changeTextLabel)
+
+  def changeTextLabel(self, text):
+    self.label.setText(text)
 
 app = QApplication(sys.argv)
 dialog = HelloWorld()
